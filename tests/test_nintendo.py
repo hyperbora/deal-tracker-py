14992585c419a3dd8e3bbb3165200036cf4ceedb
hyperbora <hyperbora@gmail.com>
2026-03-26 15:55:44 +0900

    refactor: remove unused comments

--- tests/test_nintendo.py
+++ tests/test_nintendo.py
@@ -27,19 +27,15 @@ def test_nintendo_get_price_success(mocker):
         ],
     }
 
-    # 2. requests.get 이 위 데이터를 반환하도록 가짜(Mock) 설정
     mock_get = mocker.patch("requests.get")
 
-    # [수정 포인트] return_module -> return_value
     mock_response_obj = mock_get.return_value
     mock_response_obj.status_code = 200
     mock_response_obj.json.return_value = mock_response  # json() 실행 시 데이터 반환
 
-    # 3. 실제 로직 실행
     store = NintendoStore()
     price_info = store.get_price("70010000096823")
 
-    # 4. 검증 (데이터에 맞춰서 84800으로 수정)
     assert price_info is not None
     assert price_info.regular == 84800
     assert price_info.current == 84800
